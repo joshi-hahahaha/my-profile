be667506536fe7307acdd11583d2bbecfd5764df
joshi-hahahaha <joshi.ha7@gmail.com>
2025-01-20 19:46:35 +1100

    ADD: DrifterStars background and JetBrainsMono font family.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import Navbar from "@/components/navbar/Navbar";
+import DrifterStars from "@devil7softwares/react-drifter-stars";
 
 export default function MainPage() {
   return (
-    <div>
+    <div className="relative h-screen overflow-hidden">
+      {/* DrifterStars as background */}
+      <div className="absolute inset-0 z-0">
+        <DrifterStars />
+      </div>
+      {/* Navbar on top */}
+      {/* <div className="relative z-10"> */}
       <Navbar />
+      {/* </div> */}
     </div>
   );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="text-black h-16 flex items-center px-32 justify-between ">
+    <div className="text-white h-16 flex items-center px-32 justify-between font-jbmono">
       <div>Joshi Ha</div>
       <div>Other stuff</div>
     </div>
