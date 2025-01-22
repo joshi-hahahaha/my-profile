312d5761593cbe8a70c1bed38f3bdce3fdbb46ff
joshi-hahahaha <joshi.ha7@gmail.com>
2025-01-22 16:05:09 +1100

    ADD: social buttons and links to profiles.

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { Button } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Navbar() {
   return (
     <div className="text-white h-24 flex items-center justify-between">
       <div className="text-3xl">Joshi Ha</div>
-      <div className="flex w-auto justify-between"></div>
+      <div className="flex w-auto justify-between">
+        {/* Instagram */}
+        <a
+          href="https://www.instagram.com/joshi_hahahaha?igsh=MW5kZXhjeHdibHM5aQ%3D%3D&utm_source=qr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button className="p-0">
+            <InstagramIcon
+              sx={{ color: "white", height: "48px", width: "auto" }}
+            />
+          </Button>
+        </a>
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/joshi-ha-11b339267"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button className="p-0">
+            <LinkedInIcon
+              sx={{ color: "white", height: "48px", width: "auto" }}
+            />
+          </Button>
+        </a>
+      </div>
     </div>
   );
 }
