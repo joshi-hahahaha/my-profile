5ea9ed3742773ab359e48b10dac10d5941d8c9a1
joshi-hahahaha <joshi.ha7@gmail.com>
2025-01-22 16:11:26 +1100

    ADD: added facebook because why not...

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import { Button } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function Navbar() {
   return (
     <div className="text-white h-24 flex items-center justify-between">
       <div className="text-3xl">Joshi Ha</div>
       <div className="flex w-auto justify-between">
+        {/* LinkedIn */}
+        <a
+          href="https://www.linkedin.com/in/joshi-ha-11b339267"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button className="p-0">
+            <LinkedInIcon
+              sx={{ color: "white", height: "48px", width: "auto" }}
+            />
+          </Button>
+        </a>
         {/* Instagram */}
         <a
           href="https://www.instagram.com/joshi_hahahaha?igsh=MW5kZXhjeHdibHM5aQ%3D%3D&utm_source=qr"
@@ -20,14 +33,14 @@ function Navbar() {
             />
           </Button>
         </a>
-        {/* LinkedIn */}
+        {/* Facebook */}
         <a
-          href="https://www.linkedin.com/in/joshi-ha-11b339267"
+          href="https://www.facebook.com/share/19kzC2H3WJ/?mibextid=wwXIfr"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Button className="p-0">
-            <LinkedInIcon
+            <FacebookIcon
               sx={{ color: "white", height: "48px", width: "auto" }}
             />
           </Button>
