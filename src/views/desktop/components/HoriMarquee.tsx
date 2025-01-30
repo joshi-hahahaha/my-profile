b9158cc22e30fcb5093b9caf6e807ca5479ab2a4
joshi-hahahaha <joshi.ha7@gmail.com>
2025-01-31 03:12:49 +1100

    ADD: drop shadow to marquee.

--- src/views/desktop/components/HoriMarquee.tsx
+++ src/views/desktop/components/HoriMarquee.tsx
@@ -4,7 +4,10 @@ import Marquee from "react-fast-marquee";
 function HoriMarquee() {
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
-      <Marquee speed={60} className="text-450 text-white font-bold">
+      <Marquee
+        speed={60}
+        className="text-450 text-white font-bold drop-shadow-strong"
+      >
         <div className="mr-28">JOSHI</div>
         <div className="mr-28">HA</div>
       </Marquee>
