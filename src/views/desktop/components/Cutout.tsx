242d411e0dfbf7fb4f18ef351a4d6ffcb74e1b65
joshi-hahahaha <joshi.ha7@gmail.com>
2025-01-31 04:20:05 +1100

    FIX: image positioning. now it stays on the bottom.

--- src/views/desktop/components/Cutout.tsx
+++ src/views/desktop/components/Cutout.tsx
@@ -4,14 +4,17 @@ import Image from "next/image";
 
 const Cutout = () => {
   return (
-    <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-auto h-95/100">
-      <Image
-        src={Pic}
-        alt="Cutout Image"
-        layout="intrinsic"
-        objectFit="cover"
-        priority
-      />
+    // <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-100 h-95/100">
+    <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-full h-95/100 overflow-hidden">
+      <div className="top-0 flex justify-center transform w-full">
+        <Image
+          src={Pic}
+          alt="Cutout Image"
+          layout="intrinsic"
+          objectFit="cover"
+          priority
+        />
+      </div>
     </div>
   );
 };
