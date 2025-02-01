a05237faa4c7d86f35c887adde08bf7dd3245558
joshi-hahahaha <joshi.ha7@gmail.com>
2025-02-01 17:20:14 +1100

    FIX: marquee scroll.

--- src/views/desktop/components/AboutMeMarquee.tsx
+++ src/views/desktop/components/AboutMeMarquee.tsx
@@ -3,10 +3,10 @@ import Marquee from "react-fast-marquee";
 
 function AboutMeMarquee() {
   return (
-    <div className="bg-white w-full h-20 text-black flex items-center">
+    <div className=" bg-white w-full h-20 text-black flex items-center justify-center drop-shadow-strong">
       <Marquee
         speed={60}
-        className="text-5xl text-black font-bold drop-shadow-strong italic"
+        className="text-5xl text-black font-bold drop-shadow-strong italic h-full"
       >
         <div className="mr-10">
           ABOUT ME • ABOUT ME • ABOUT ME • ABOUT ME • ABOUT ME • ABOUT ME •
